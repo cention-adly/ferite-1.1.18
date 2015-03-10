d88b65a05d818e89f12eda3ae9a5e98092e833ea
Nazri Ramliy <nazri_ramliy@cention.asia>
2015-03-10 14:36:42 +0800

    Add more test to ferite_utils-replace-string.c
    Note: test_replace_huge_string2 may cause a SEGFAULT if N is
    sufficiently large. On my machine setting N to 3*128*1024*1024 and with
    appropriate modifications to the function so that txt is a pointer to
    the static array (defined outside of the function):
    
      $ gdb .libs/lt-ferite_utils-replace-string.t
      Reading symbols from /home/nazri/work/c3-pristine/cention-ferite/test/.libs/lt-ferite_utils-replace-string.t...done.
      (gdb) run
      Starting program: /home/nazri/work/c3-pristine/cention-ferite/test/.libs/lt-ferite_utils-replace-string.t
      warning: no loadable sections found in added symbol-file system-supplied DSO at 0x7ffff7ffa000
      [Thread debugging using libthread_db enabled]
      Using host libthread_db library "/lib/x86_64-linux-gnu/libthread_db.so.1".
      ok 1 - Replace '' in 'Hello world' with '' must become 'Hello world'
      ...
      ok 17 - replace_huge_string1
    
      Program received signal SIGSEGV, Segmentation fault.
      0x00007ffff7b29a59 in ferite_jedi_morecore (bucket=30) at ferite_mem_jedi.c:465
      465         ((FeriteMemoryChunkHeader*)new_block)->storage.next = ferite_jedi_big_chunks; /* hook up  header */
      (gdb) bt
      #0  0x00007ffff7b29a59 in ferite_jedi_morecore (bucket=30) at ferite_mem_jedi.c:465
      #1  0x00007ffff7b29175 in ferite_jedi_malloc (size=603979777, file=0x7ffff7b65649 "ferite_mem_jedi.c", line=288,
          script=0x0) at ferite_mem_jedi.c:232
      #2  0x00007ffff7b293bb in ferite_jedi_calloc (size=603979777, blk_size=1, file=0x7ffff7bbf0c0 "ferite_utils.c",
          line=267, script=0x0) at ferite_mem_jedi.c:288
      #3  0x00007ffff7b50e52 in ferite_replace_string (
          str=0x6022e0 <TXT> "AaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAa
      AaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAa"...,
          pattern=0x4013ea "A", data=0x401401 "BB") at ferite_utils.c:267
      #4  0x0000000000400bc0 in test_replace_huge_string2 () at ferite_utils-replace-string.c:164
      #5  0x0000000000400d0f in main (argc=1, argv=0x7fffffffe248) at ferite_utils-replace-string.c:196
      (gdb) quit
    
    It seems like ferite_jedimore_core is buggy (yea it's always easiest to
    put the blame on someone else's code).

--- test/ferite_utils-replace-string.c
+++ test/ferite_utils-replace-string.c
@@ -113,12 +113,9 @@ struct TestVector tests[] = {
 	},
 };
 
-int main(int argc, char *argv[])
-{
+void test_replace() {
 	size_t i;
 	size_t ntests = sizeof(tests) / sizeof(tests[0]);
-
-	ferite_init(argc, argv);
 
 	for (i = 0; i < ntests; i++) {
 		struct TestVector *t = &tests[i];
@@ -127,6 +124,75 @@ int main(int argc, char *argv[])
 		is_str(got, t->want, "Replace '%s' in '%s' with '%s' must become '%s'",
 				t->replace, t->text, t->with, t->want);
 	}
+}
+
+#define NCHARS (1024*1024)
+#define N (NCHARS+1)
+void test_replace_huge_string1() {
+	size_t i;
+
+	char txt[N];
+	for (i = 0; i < NCHARS; i++) {
+		txt[i] = i % 2 ? 'a':'A';
+	}
+	txt[i] = '\0';
+
+	char *got = ferite_replace_string(txt, "A", "B");
+	int good = 1;
+	for (i = 0; i < NCHARS; i++) {
+		char want = i % 2 ? 'a':'B';
+		if (got[i] != want) {
+			good = 0;
+		}
+	}
+
+	ok(good, "replace_huge_string1");
+}
+
+#define NCHARS (1024*1024)
+#define N (NCHARS+1)
+void test_replace_huge_string2() {
+	size_t i;
+
+	char txt[N];
+	for (i = 0; i < NCHARS; i++) {
+		txt[i] = i % 2 ? 'a':'A';
+	}
+	txt[i] = '\0';
+
+	char *got = ferite_replace_string(txt, "A", "BB");
+	//printf("txt %d[%s]\n",strlen(txt), txt);
+	//printf("got %d[%s]\n",strlen(got),  got);
+	int gotLen = strlen(got);
+	is (gotLen, NCHARS + NCHARS / 2, "Length of new text must be 1.5 times the previous one");
+	int good = 1;
+	for (i = 0; i < NCHARS; i++) {
+		if (i%2) {
+			// 'a' must not be replaced
+			int j = i + i / 2 + 1;
+			if (got[j] != 'a') {
+				diag("FAIL: got[%d] != 'a'!", j);
+				good = 0;
+			}
+		} else {
+			int j = i + i / 2;
+			if (strncmp("BB", got + j, 2) != 0) {
+				diag("FAIL: got[%d:%d] must be BB", j, j+2);
+				good = 0;
+			}
+		}
+	}
+
+	ok(good, "ferite_replace_string2");
+}
+
+int main(int argc, char *argv[])
+{
+	ferite_init(argc, argv);
+
+	test_replace();
+	test_replace_huge_string1();
+	test_replace_huge_string2();
 
 	return done_testing();
 }
